0374b9a56306f08a0c17291b64b84d84d9d9dda3
1102137205 <apple41632@gmail.com>
2016-03-23 20:25:57 +0800

    新增SearchController 及 View

---
+++ WorkShop/Controllers/SearchDataController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WorkShop.Controllers
+{
+    public class SearchDataController : Controller
+    {
+        // GET: SearchData
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
