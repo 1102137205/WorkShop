dcb75913007d424aa6c426a9858f1ad97e629147
1102137205 <apple41632@gmail.com>
2016-04-07 16:32:04 +0800

    資料修改頁面 填入日期 並且增加修改功能

--- WorkShop/Controllers/OrderController.cs
+++ WorkShop/Controllers/OrderController.cs
@@ -64,67 +64,75 @@ namespace WorkShop.Controllers
             return View();
         }
 
-        [HttpPost]
-        public ActionResult search(FormCollection inputs)
-        {
-            String orderId = inputs["OrderId"];
-            return RedirectToAction("update", new { orderId });
-        }
 
         public ActionResult update(String orderId)
         {
-
+            
             #region 填入 table 資料
             List<Orders> OrderData = db.Orders.Where(x => x.OrderID.ToString() == orderId).ToList();
+
             Orders data = new Orders();
 
-            String CompanyName = "";
+            String CustomersCompanyName = "";
             String LastName = "";
+            String ShippersCompanyName = "";
 
             String OrderDate = "";
             String orderMonth = "";
             String orderDay = "";
-            //String RequiredDate = "";
-            //String ShippedDate = "";
 
+            String RequiredDate = "";
+            String requiredMonth = "";
+            String requiredDay = "";
+
+            String ShippedDate = "";
+            String shippedMonth = "";
+            String shippedDay = "";
 
             foreach (var tmp in OrderData) {
                 data.OrderID = tmp.OrderID;
-                CompanyName = tmp.Customers.CompanyName;
+                CustomersCompanyName = tmp.Customers.CompanyName;
                 LastName = tmp.Employees.LastName;
-
+                
                 orderMonth = DateFormat(tmp.OrderDate.Month.ToString());
                 orderDay = DateFormat(tmp.OrderDate.Day.ToString());
-
                 OrderDate = String.Format("{0}-{1}-{2}", tmp.OrderDate.Year, orderMonth, orderDay);
 
-                //date.GetDateTimeFormats('s')[0].ToString();
-                //data.RequiredDate = tmp.RequiredDate;
-                //data.ShippedDate = tmp.ShippedDate;
+                requiredMonth = DateFormat(tmp.RequiredDate.Month.ToString());
+                requiredDay = DateFormat(tmp.RequiredDate.Day.ToString());
+                RequiredDate = String.Format("{0}-{1}-{2}", tmp.RequiredDate.Year, requiredMonth, requiredDay);
 
-                data.ShipName = tmp.ShipName;
+                shippedMonth = DateFormat(Convert.ToDateTime(tmp.ShippedDate).Month.ToString());
+                shippedDay = DateFormat(Convert.ToDateTime(tmp.ShippedDate).Day.ToString());
+                ShippedDate = String.Format("{0}-{1}-{2}", Convert.ToDateTime(tmp.ShippedDate).Year, shippedMonth, shippedDay);
+
+                ShippersCompanyName = tmp.Shippers.CompanyName;
+
                 data.Freight = tmp.Freight;
                 data.ShipCountry = tmp.ShipCountry;
                 data.ShipCity = tmp.ShipCity;
                 data.ShipRegion = tmp.ShipRegion;
                 data.ShipPostalCode = tmp.ShipPostalCode;
                 data.ShipAddress = tmp.ShipAddress;
+                data.ShipName = tmp.ShipName;
             }
-
-
 
             ViewBag.OrderData = data;
             ViewBag.LastName = LastName;
-            ViewBag.CompanyName = CompanyName;
-            ViewBag.OrderDate = OrderDate;
+            ViewBag.CustomersCompanyName = CustomersCompanyName;
+            ViewBag.ShippersCompanyName = ShippersCompanyName;
 
-            List<String> CompanyNameList = db.Customers.Where(x => x.CompanyName != CompanyName).Select(x => x.CompanyName).ToList();
+            ViewBag.OrderDate = OrderDate;
+            ViewBag.RequiredDate = RequiredDate;
+            ViewBag.ShippedDate = ShippedDate;
+
+            List<String> CompanyNameList = db.Customers.Where(x => x.CompanyName != CustomersCompanyName).Select(x => x.CompanyName).ToList();
             ViewBag.CompanyNameList = CompanyNameList;
 
             List<String> lastNameList = db.Employees.Where(x => x.LastName != LastName).Select(x => x.LastName).ToList();
             ViewBag.lastNameList = lastNameList;
 
-            List<String> shipNameList = db.Orders.Where(x=>x.ShipName != data.ShipName  ).Select(x => x.ShipName).ToList();
+            List<String> shipNameList = db.Shippers.Where(x => x.CompanyName != ShippersCompanyName).Select(x => x.CompanyName).ToList();
             ViewBag.shipNameList = shipNameList;
 
             #endregion
@@ -148,18 +156,70 @@ namespace WorkShop.Controllers
             return View();
         }
 
-        public String DateFormat(String date) {
+        public String DateFormat(String date)
+        {
 
             StringBuilder sb = new StringBuilder();
 
             if (date.Length < 2)
             {
                 sb.Append("0");
-                sb.Append(date);
             }
+            sb.Append(date);
 
             return sb.ToString();
         }
+
+        [HttpPost]
+        public ActionResult update(FormCollection inputs) {
+
+            String orderId = inputs["orderId"];
+            String custName = inputs["custName"];
+            String employeeName = inputs["employeeName"];
+            String orderDate = inputs["orderDate"];
+            String needDate = inputs["needDate"];
+            String shipDate = inputs["shipDate"];
+            String shipCompanyName = inputs["shipCompanyName"];
+            String shipCost = inputs["shipCost"];
+            String shipCountry = inputs["shipCountry"];
+            String shipCity = inputs["shipCity"];
+            String shipRegion = inputs["shipRegion"];
+            String shipAddressNo = inputs["shipAddressNo"];
+            String shipAddress = inputs["shipAddress"];
+            String shipDesc = inputs["shipDesc"];
+
+            List<Orders> data = db.Orders.Where(x => x.OrderID.ToString() == orderId).ToList();
+
+            foreach(var tmp in data){
+                tmp.Customers.CompanyName = custName;
+                tmp.Employees.LastName = employeeName;
+                tmp.OrderDate = Convert.ToDateTime(orderDate);
+                tmp.RequiredDate = Convert.ToDateTime(needDate);
+                tmp.ShippedDate = Convert.ToDateTime(shipDate);
+                tmp.Shippers.CompanyName = shipCompanyName;
+                tmp.Freight = Convert.ToDecimal(shipCost);
+                tmp.ShipCountry = shipCountry;
+                tmp.ShipCity = shipCity;
+                tmp.ShipRegion = shipRegion;
+                tmp.ShipPostalCode = shipAddressNo;
+                tmp.ShipAddress = shipAddress;
+                tmp.ShipName = shipDesc;
+            }
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult resultUpdate(String orderId) {
+
+
+
+            return View();
+        }
+
+
+        
 
     }
 }
